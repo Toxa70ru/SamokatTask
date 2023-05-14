5c825a20f0f1b03fd8e856d40d094f05313a9307
anton <anton@REDMI-BOOK>
2023-05-14 15:59:52 +0700

    Добавьте файлы проекта.

---
+++ Controllers/RanksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SamokatTask.Data.Interfaces;
+
+namespace SamokatTask.Controllers
+{
+    public class RanksController : Controller
+    {
+        public readonly IRank _rank;
+
+        public RanksController(IRank irank)
+        {
+            _rank = irank;
+        }
+        public ViewResult List() {
+            var ran = _rank.ranks;
+            return View(ran);
+        }
+    }
+}
---
+++ Data/mocks/MockRanks.cs
@@ -0,0 +1,51 @@
+﻿using SamokatTask.Data.Interfaces;
+using SamokatTask.Data.Models;
+using System.Data;
+
+namespace SamokatTask.Data.mocks
+{
+    public class MockRanks : IRank
+    {
+        public IEnumerable<Rank> ranks {
+            get {
+                return new List<Rank> {
+                    new Rank
+                    {
+                        Id = 1,
+                        Img = "",
+                        Name = "Иванов Иван Иванович",
+                        Level = 9
+                    },
+                    new Rank
+                    {
+                        Id = 2,
+                        Img = "",
+                        Name = "Сидоров Сегрей Сергеевич",
+                        Level = 8
+                    },
+                    new Rank
+                    {
+                        Id = 3,
+                        Img = "",
+                        Name = "Хавелев Илья Викторович",
+                        Level = 7
+                    },
+                    new Rank
+                    {
+                        Id = 4,
+                        Img = "",
+                        Name = "Даниленко Фёдор Андреевич",
+                        Level= 6
+                    },
+                    new Rank
+                    {
+                        Id = 5,
+                        Img = "",
+                        Name = "Базаев Роман Георгиевич",
+                        Level= 5
+                    }
+                };
+            }
+        }
+    }
+}
